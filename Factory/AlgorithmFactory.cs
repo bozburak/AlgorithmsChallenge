8e0e28c21d64d829b3292b987996a14f824a4c8e
bozburak <boz-burak@hotmail.com>
2021-09-14 09:12:42 +0300

    Updated factory cs

--- Factory/AlgorithmFactory.cs
+++ Factory/AlgorithmFactory.cs
@@ -9,7 +9,7 @@ namespace Factory
         public static IAlgorithm GetAlgorithm(string algorithmName)
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
-            string type = assembly.GetType(algorithmName).FullName;
+            string type = assembly.GetType("Concretes." + algorithmName).FullName;
             return (IAlgorithm)Activator.CreateInstanceFrom(assembly.Location, type).Unwrap();
         }
     }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Main
     {
         static void Main()
         {
-            IAlgorithm algroithm = AlgorithmFactory.GetAlgorithm("Concretes.BinaryGap");
+            IAlgorithm algroithm = AlgorithmFactory.GetAlgorithm("BinaryGap");
             algroithm.Algroithm();
         }
     }
